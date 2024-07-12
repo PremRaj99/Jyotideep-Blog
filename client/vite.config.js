98f7bb2a8090a7826836ddb9bbbd6b878955e2f5
Prem Raj <premraj299341@gmail.com>
2024-07-12 17:02:26 +0530

    fix some error

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://jyotideep-blog-api.vercel.app/',
+        target: 'https://jyotideep-blog-api.vercel.app',
         secure: false,
       },
     },
