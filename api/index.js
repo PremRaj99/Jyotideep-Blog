22b8501cc0571ed9be99901c71abdd8c1945453e
Prem Raj <premraj299341@gmail.com>
2024-07-12 20:02:40 +0530

    prepare for deploying in render

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -19,16 +19,11 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-// Set up CORS to allow requests from your frontend's domain
-app.use(cors({
-  origin: 'https://www.nayasaveraparivar.life', // Replace with your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
 
 app.listen("3000", () => {
   console.log("server is running on port 3000");
@@ -39,9 +34,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//   },
+// })
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
   },
