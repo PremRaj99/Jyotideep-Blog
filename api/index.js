99b609d23e39e4638358411f72e0922e6a09a507
Prem Raj <premraj299341@gmail.com>
2024-07-12 16:55:34 +0530

    some change for deploying frontend

--- api/index.js
+++ api/index.js
@@ -38,6 +38,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";
