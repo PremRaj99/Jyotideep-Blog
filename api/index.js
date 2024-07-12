d1083018651229cf12e171301aee6af535e26110
Prem Raj <premraj299341@gmail.com>
2024-07-12 16:51:55 +0530

    make frontend and backend seperately

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-// import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,11 +19,15 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST, PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.listen("3000", () => {
   console.log("server is running on port 3000");
@@ -33,12 +37,6 @@ app.use("/api/user", UserRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
-
-// app.use(express.static(path.join(__dirname, "/client/dist")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
-// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
