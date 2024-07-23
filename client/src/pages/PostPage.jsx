469984756843398e3fafa31e794d98bd3e6bda20
Prem Raj <premraj299341@gmail.com>
2024-07-23 19:32:42 +0530

    add more content in it

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -60,13 +60,13 @@ export default function PostPage() {
 
   const handleWhatsappShare = () => {
     const postLink =
-      "https://nayasaveraparivar.life/post/%E0%A4%A8%E0%A4%AF%E0%A4%BE-%E0%A4%B8%E0%A4%B5%E0%A5%87%E0%A4%B0%E0%A4%BE-%E0%A4%AA%E0%A4%B0%E0%A4%BF%E0%A4%B5%E0%A4%BE%E0%A4%B0-%E0%A4%95%E0%A5%80-%E0%A4%A6%E0%A4%BE%E0%A4%B0%E0%A5%8D%E0%A4%B6%E0%A4%A8%E0%A4%BF%E0%A4%95-%E0%A4%AE%E0%A4%BE%E0%A4%A8%E0%A5%8D%E0%A4%AF%E0%A4%A4%E0%A4%BE%E0%A4%8F%E0%A4%81";
+      `https://nayasaveraparivar.life/post/${postSlug}`;
     const plainTextContent = htmlToText(post.content, {
       wordwrap: 130,
       limits: {
         maxInputLength: 10000,
       },
-    }).slice(0, 300);
+    }).slice(0, 600);
     const text = "```" + plainTextContent + "```";
     const message = `*${post.title}*\n\n${text}...\n\nSee more: ${postLink}`;
     const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(
