5c3f9fd19fa26d0ad52a88c0249d19a3f1249c63
Prem Raj <premraj299341@gmail.com>
2024-10-07 03:02:00 +0530

    update backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,6 +58,7 @@ export const signin = async (req, res, next) => {
       .status(200)
       .cookie("access_token", token, {
         httpOnly: true,
+        sameSite: "None",
       })
       .json(rest);
   } catch (error) {
