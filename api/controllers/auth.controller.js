cb1869253fb83ade0d1d652ef2a270b23b44c297
Prem Raj <premraj299341@gmail.com>
2025-10-18 00:25:52 +0530

    fix: change variable declaration from const to let for user in google authentication; update new user creation logic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const signin = async (req, res, next) => {
 export const google = async (req, res, next) => {
   const { name, email, googlePhotoUrl } = req.body;
   try {
-    const user = await User.findOne({ email });
+    let user = await User.findOne({ email });
     if (user) {
       const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
@@ -88,7 +88,7 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(gereratePassword, 10);
-      const newUser = new User({
+      user = new User({
         username:
           name.toLowerCase().split(" ").join("") +
           Math.random().toString(9).slice(-4),
